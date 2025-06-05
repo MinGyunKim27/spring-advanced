02901a6e5d68d99f5d7a9b2bf16f40969ff8c770
김민균 <kmg02845@gmail.com>
2025-06-05 11:23:58 +0900

    과제 LV1-3 코드 개선 퀴즈 - Validation 구현 완료

--- src/main/java/org/example/expert/domain/user/dto/request/UserChangePasswordRequest.java
+++ src/main/java/org/example/expert/domain/user/dto/request/UserChangePasswordRequest.java
@@ -1,6 +1,8 @@
 package org.example.expert.domain.user.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -11,6 +13,11 @@ import lombok.NoArgsConstructor;
 public class UserChangePasswordRequest {
 
     @NotBlank
+    @Size(min = 8)
+    @Pattern(
+            regexp = "^(?=.*\\d)(?=.*[A-Z]).*$",
+            message = "비밀번호에는 숫자와 대문자가 각각 하나 이상 포함되어야 합니다."
+    )
     private String oldPassword;
     @NotBlank
     private String newPassword;
